90415ae8c2bd3f3e8eb9cf8ac69c0b7ba2a88efa
salemalhamed2007 <42217524+salemalhamed2007@users.noreply.github.com>
2018-09-14 20:47:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2191,7 +2191,7 @@ if (message.content.startsWith(prefix + 'نقاطي')) {
   fs.writeFile("./json/awsmm.json", JSON.stringify(points), (err) => {
     if (err) console.error(err)
   })
-});*/
+});/
 
 /*nickname*/
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
